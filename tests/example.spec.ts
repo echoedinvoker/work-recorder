8f932370a8fd5ae37d9135b3775b3724a9ca3270
echoedinvoker <echoedinvoker@gmail.com>
2025-06-29 09:01:37 +0000

    fix goto url

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('http://localhost:4173');
   const header = page.locator('h1');
   await expect(header).toHaveText('工作時間記錄器');
 });
