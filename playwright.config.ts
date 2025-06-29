80b55ef79f84df1a17fedb59d87c5eecb8f2b8fa
echoedinvoker <echoedinvoker@gmail.com>
2025-06-29 09:32:07 +0000

    Integrate Argos to Github CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
-  reporter: 'html',
+  reporter: [
+    // Use "dot" reporter on CI, "list" otherwise (Playwright default).
+    process.env.CI ? ["dot"] : ["list"],
+    // Add Argos reporter.
+    [
+      "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+
+        // Set your Argos token (required if not using GitHub Actions).
+        // token: "<YOUR-ARGOS-TOKEN>",
+      },
+    ],
+  ],
   use: {
+    // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
-    baseURL: 'http://localhost:5173',
+
+    // Capture screenshot after each test failure.
+    screenshot: "only-on-failure", baseURL: 'http://localhost:5173',
   },
   projects: [
     {
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { argosScreenshot } from "@argos-ci/playwright";
 
 test('has title', async ({ page }) => {
   await page.goto('/');
+  await argosScreenshot(page, "homepage");
   const header = page.locator('h1');
   await expect(header).toHaveText('工作時間記錄器');
 });
